59456dd67e6488193323c0f4e70431eac1f42ba6
jakehanson <jake.hanson@asu.edu>
2018-01-11 12:41:15 -0700

    algorithm comparison

---
+++ ALGORITHM_COMPARISON/SLOPE_LIMITERS/header.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <vector>
+#include <iostream>
+#include <random>
+#include <stdio.h>
+#include <map>
+#include <stdexcept>
+#include <fstream>
+#include <cmath>
+
+
+double minmod(double a, double b);
+double maxmod(double a, double b);
+std::ostream &operator<<(std::ostream &out, std::vector<double> q);
---
+++ ALGORITHM_COMPARISON/SLOPE_LIMITERS/main.cpp
@@ -0,0 +1,54 @@
+#include "header.h"
+
+int main(int argc, char** argv)
+{
+	int nx = 100; // size of domain
+	double u = 1.0; // velocity
+	double dt = 0.1; // timestep
+	int N_steps = 300; // number of steps
+	//int N_steps = 1; // number of steps
+	double dx = 1.0; // grid size
+
+	std::vector<double> q(nx,0); // quantity to advect
+	std::vector<double> sigma(nx,0); // slope at each cell
+	double slope1,slope2; // used for slope limiter
+
+	// Open file to store results
+	std::ofstream data("data.txt");
+
+	// Initialize Step Function
+	q.at(0) = 0.;
+	for(int i=1;i<=30;i++){
+		q.at(i) = 1.0;
+	}
+
+	std::cout << "STARTING.." << std::endl;
+	data << q << std::endl;
+
+	// Evolve q through N steps
+	for(int i=0;i<N_steps;i++){
+
+		// Get slope at each cell (except boundary where slope = 0 and we use simple donor cell)
+		for(int j=1;j<sigma.size()-1;j++){
+			slope1 = (q.at(j+1)-q.at(j))/dx;
+			slope2 = (q.at(j)-q.at(j-1))/dx;
+			//sigma.at(j)=maxmod(minmod(slope1,2*slope2),minmod(2*slope1,slope2)); // SUPERBEE
+			sigma.at(j) = slope1; // LAX-WENDROFF (DOWNWIND SLOPE)
+			//sigma.at(j) = 0.; // DONOR CELL
+
+
+		}
+
+		// Evolve all cells but boundary (assumes u > 0 since it only uses j,j-1)
+		for(int j=1;j<q.size();j++){
+			q.at(j) = q.at(j) - u*dt/dx*(q.at(j)-q.at(j-1))-u*dt/(2.*dx)*(sigma.at(j)-sigma.at(j-1))*(dx-u*dt);
+		}
+
+		data << q << std::endl;
+
+	}
+	
+	data.close();
+	std::cout << "COMPLETE" << std::endl;
+
+}
